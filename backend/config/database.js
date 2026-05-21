78ed5e5788a8a95720fecca5c50610e5d16f0829
Disha2628 <dishadange2603@gmail.com>
2026-05-21 12:45:14 +0530

    hosted database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
+const fs = require('fs');
 
 const dbConfig = {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('./ca.pem')
+  }
 };
 
 const pool = mysql.createPool(dbConfig);
