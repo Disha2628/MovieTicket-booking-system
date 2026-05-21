8cd686688532b485abab5466a4726b596c27aef6
Disha2628 <dishadange2603@gmail.com>
2026-05-21 16:31:40 +0530

    no comments

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(passport.session());
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://movie-ticket-booking-system.vercel.app'
+    'client-movieticket-booking-system.vercel.app'
   ],
   credentials: true
 }));
